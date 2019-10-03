5fc579b71360f29adb656dbaa6f3e8ccc0c2f55d
JosephRojas <josephrojas21@hotmail.com>
2019-10-02 20:02:02 -0500

    fists project done

--- Ejercicios/binding/src/App.js
+++ Ejercicios/binding/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      campo: ''
+    }
+  }
+  
+  
+
+  render() {
+    const { campo } = this.state
+    return (
+      <Fragment>
+        <div className="container">
+
+          <h5 className="grey-text text-darken-3">Primer ejercicio</h5>
+          <div className="input-field">
+            <p>ingrese texto</p>
+            <input value={campo} onChange={(e)=> this.setState({ campo: e.target.value })} />
+          </div>
+          
+            <p>este es tu texto {campo}</p>
+          
+        </div>
+      </Fragment>
+
+    )
+  }
+
 }
 
 export default App;
