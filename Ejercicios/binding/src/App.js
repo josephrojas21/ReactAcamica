bbac5d09a3dca4e920e8b136ce22fcbd8ad4d82f
JosephRojas <josephrojas21@hotmail.com>
2019-10-21 06:54:12 -0500

    proyecto hoteles iniciado

--- Ejercicios/binding/src/App.js
+++ Ejercicios/binding/src/App.js
@@ -2,19 +2,46 @@ import React, { Component, Fragment } from 'react';
 
 import './App.css';
 
+
+
 class App extends Component {
 
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
     this.state = {
-      campo: ''
+      value: '',
+      value2:'',
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.deleteInput = this.deleteInput.bind(this,'value');
+    this.deleteSecondInput = this.deleteSecondInput.bind(this, 'value2');
   }
+
+  handleChange(e){
+    const {name, value } = e.target;
+    this.setState({[name]: value})
+  }
+
+  deleteInput(...arg){
+    console.log(arg[0]);
+    console.log(arg[1]);
+    this.setState({value: ''})
+  }
+
+  deleteSecondInput(valor){
+    console.log(valor);
+    this.setState({value2: ''})
+    
+  }
+
+  
   
   
 
   render() {
-    const { campo } = this.state
+    const{value, value2} = this.state;
+    const copiaDelState = {...this.state, value3:'dede'}
     return (
       <Fragment>
         <div className="container">
@@ -22,10 +49,17 @@ class App extends Component {
           <h5 className="grey-text text-darken-3">Primer ejercicio</h5>
           <div className="input-field">
             <p>ingrese texto</p>
-            <input value={campo} onChange={(e)=> this.setState({ campo: e.target.value })} />
+            <input value={copiaDelState.value} onChange={this.handleChange} name="value"/>
+            <input value={copiaDelState.value2} onChange={this.handleChange} name="value2" />
+
           </div>
-          
-            <p>este es tu texto {campo}</p>
+            <button onClick={this.deleteInput}>
+                    borrar
+            </button>
+            <button onClick={this.deleteSecondInput}>
+                    borrar
+            </button>
+            <p>este es tu texto {value}................ {value2}........{value3}</p>
           
         </div>
       </Fragment>
