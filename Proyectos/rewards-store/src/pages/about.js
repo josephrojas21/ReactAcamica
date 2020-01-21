8004e5c548e134bbc45550b8363dc85f2b3f9d8e
joseph rojas <josephrojas21@hotmail.com>
2020-01-21 17:11:17 -0500

    config redux, jest  and enzime

---
+++ Proyectos/rewards-store/src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Page from '../components/page';
+
+ const About = () => (
+    <div>
+        <div>
+            <h1>About Page</h1>
+        </div>
+        <style jsx>{`
+        
+        `}</style>
+    </div>
+)
+
+export default Page(About);
